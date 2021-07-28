b3c952c4dbb800adae4559818401c2d7f73af6d7
Stanly Lau <stanly@odd-e.com>
2021-07-28 10:15:59 +0800

    End of 1st session dojo

--- src/fizz_buzz.test.js
+++ src/fizz_buzz.test.js
@@ -1,11 +1,73 @@
 import say from './fizz_buzz'
 
-/**
- * Please write unit tests to cover the logic of say().
- * Tip: You may read Common Matchers section in https://jestjs.io/docs/en/using-matchers for reference
- */
+class Cart{
+  constructor(inventory){
+    this.inventory = inventory
+    this.productList = []
+  }
+   
+  scan(sku){
+    this.productList.push(sku)
+  }
+
+  checkout() {
+    if(this.inventory.items.length === 2)
+      return 0.5
+    return this.inventory.item.unitPrice * this.productList.length
+  }
+}
+
+class Item{
+  constructor(sku, name, unitPrice){
+    this.sku = sku
+    this.name = name
+    this.unitPrice = unitPrice
+  }
+}
+
+class Inventory{
+  constructor() {
+    this.item = null
+    this.items = []
+   }
+  add(sku, name, unitPrice){
+    this.item = new  Item(sku, name, unitPrice)
+    this.items.push(new  Item(sku, name, unitPrice))
+  }
+}
+
+
 describe("Fizz Buzz", () => {
-  it('a sample test', () => {
-    expect(true).toBeTruthy()
+  let inventory
+  let cart
+
+  beforeEach(() => {  
+    inventory = new Inventory()
+    cart = new Cart(inventory)
+  })
+
+  it('scan one product 1 time', () => {
+    inventory.add('1', 'apple', 0.3)
+    cart.scan('1')
+    
+    expect(cart.checkout()).toBe(0.30)
+  })
+
+  it('scan 1 product 3 times', () => {
+    inventory.add('1', 'apple', 0.5)
+    cart.scan('1')
+    cart.scan('1')
+    cart.scan('1')
+
+    expect(cart.checkout()).toBe(1.5)
+  })
+
+  it('2 product', () => {
+    inventory.add('1', 'apple', 0.5)
+    inventory.add('2', 'orange', 0.3)
+    
+    cart.scan('1')
+
+    expect(cart.checkout()).toBe(0.5)
   })
 })
